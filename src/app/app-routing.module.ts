e5d7a915c859ea5c5c5317563d1d387fa005afc9
M-Maros-DA <maros.mate@dozsakalocsa.hu>
2025-12-03 13:17:28 +0100

    Regiszter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BejelentkezesComponent } from './bejelentkezes/bejelentkezes.component';
+import { RegisztracioComponent } from './regisztracio/regisztracio.component';
 
-const routes: Routes = [{path:'',component:BejelentkezesComponent}];
+const routes: Routes = [
+  {path:'',component:BejelentkezesComponent},
+  {path:'register', component:RegisztracioComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
