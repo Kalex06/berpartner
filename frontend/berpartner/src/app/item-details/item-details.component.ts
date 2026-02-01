1f8929c2fdb1851270f6ad885672cd63b23f96d4
M-Maros-DA <maros.mate@dozsakalocsa.hu>
2026-02-02 00:36:30 +0100

    Bérelhető dolgok részleteinek oldalának a dizájnja

--- frontend/berpartner/src/app/item-details/item-details.component.ts
+++ frontend/berpartner/src/app/item-details/item-details.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NotificationsComponent } from '../notifications/notifications.component';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
+
+interface SortOption {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-item-details',
@@ -6,5 +15,27 @@ import { Component } from '@angular/core';
   styleUrl: './item-details.component.css'
 })
 export class ItemDetailsComponent {
+  constructor(private auth: AuthService, private router: Router) { }
 
+  readonly dialog = inject(MatDialog);
+
+  openNotifications() {
+    this.dialog.open(NotificationsComponent);
+  }
+
+  navigateBack() {
+    setTimeout(() => {
+      this.router.navigate(['/home']);
+    }, 400);
+  }
+
+  options: SortOption[] = [
+    { value: 'newest-0', viewValue: 'Legújabb' },
+    { value: 'price-desc', viewValue: 'Legdrágább felül' },
+    { value: 'price-asc', viewValue: 'Legolcsóbb felül' },
+  ];
+  selectedSort: string = 'newest-0';
+  
 }
+
+
