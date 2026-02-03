3ee5f66669c2887487353a89842322b13307413b
Kalex06 <kopo.alex@dozsakalocsa.hu>
2026-02-03 23:47:28 +0100

    kép feltöltés backend (nem tesztelt)

--- backend/backend/controllers/item.controller.js
+++ backend/backend/controllers/item.controller.js
@@ -1,11 +1,48 @@
 const Item = require('../models/item.model');
+const Category = require('../models/category.model');
 
 async function uploadItem(req,res){
     try{
         
+        const {kategoria} = req.body;
+         const{nev,ar_egy_napra,allapot,leiras} = req.body;
 
+        const categoryId = await Category.findCategory(kategoria);
+
+       
+
+        const itemData = {
+            nev:nev,
+            kategoria_id:categoryId,
+            ar_egy_napra:ar_egy_napra,
+            allapot:allapot,
+            leiras:leiras,
+            tulajdonos_id: req.user.id
+        };
+
+       const savedItemdata = await Item.uploadItem(itemData);
+
+
+
+        const {eszkoz_id} = req.body;
+        const pictures = [];
+
+        for (let index = 0; index < req.files.length; index++) {
+            const element = req.files[index];
+            pictures.push({
+                eszkoz_id: eszkoz_id,
+                eleresi_ut: element.filename
+            });
+
+
+        };
+
+      const savedPic = await Item.uploadpictures(pictures);
+        res.status(200).json({message:`Sikeres feltöltés! Adat: ${savedItemdata}  Képek: ${savedPic}`});
     }
     catch(err){
-
+            res.status(500).json({message:"Hiba feltöltés közben"});
     }
 }
+
+module.exports = {uploadItem}
--- backend/backend/models/item.model.js
+++ backend/backend/models/item.model.js
@@ -2,12 +2,26 @@ const pool = require('../config/db');
 
 
 
-async function uploadpictures(item_id,path) {
-    const [rows] = await pool.execute(
-        'INSERT INTO termek_kepek (eszkoz_id,eleresi_ut) VALUES (?,?)',
-        [item_id,path]
+async function uploadpictures(data) {
+    const [rows] = await pool.query(
+        'INSERT INTO eszkoz_kepek (eszkoz_id,eleresi_ut) VALUES (?)',
+        [data]
     );
+     return rows.fieldCount;
+    
+ }
+
+ async function uploadItem(data) {
+    const {nev,kategoria_id,ar_egy_napra,allapot,leiras,tulajdonos_id} = data;
+    const [rows] = await pool.execute(
+        'INSERT INTO eszkozok (nev,kategoria_id,ar_egy_napra,allapot,leiras,tulajdonos_id) VALUES (?,?,?,?,?,?)',
+        [nev,kategoria_id,ar_egy_napra,allapot,leiras,tulajdonos_id]
+    );
+    return rows.fieldCount;
  }
 
 
-module.exports = {uploadpictures}
+
+
+
+module.exports = {uploadpictures,uploadItem}
