0228060e74394cfbec2901137fca5b69800b1973
M-Maros-DA <maros.mate@dozsakalocsa.hu>
2026-02-04 01:20:52 +0100

    Bérlési időszak naptáros bejelölése

--- frontend/berpartner/src/app/item-details/item-details.component.ts
+++ frontend/berpartner/src/app/item-details/item-details.component.ts
@@ -1,6 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { NotificationsComponent } from '../notifications/notifications.component';
-import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from '../services/auth/auth.service';
 import { Router } from '@angular/router';
 
@@ -17,12 +15,6 @@ interface SortOption {
 export class ItemDetailsComponent {
   constructor(private auth: AuthService, private router: Router) { }
 
-  readonly dialog = inject(MatDialog);
-
-  openNotifications() {
-    this.dialog.open(NotificationsComponent);
-  }
-
   navigateBack() {
     setTimeout(() => {
       this.router.navigate(['/home']);
@@ -35,7 +27,33 @@ export class ItemDetailsComponent {
     { value: 'price-asc', viewValue: 'Legolcsóbb felül' },
   ];
   selectedSort: string = 'newest-0';
-  
+
+
+  // Bérlés időszaka
+  startDate: Date | null = null;
+  endDate: Date | null = null;
+  rangeText: string = 'Bérlési időszak kiválasztása';
+
+  onStartChange(event: any) {
+    this.startDate = event.value;
+    this.updateRangeText();
+  }
+
+  onEndChange(event: any) {
+    this.endDate = event.value;
+    this.updateRangeText();
+  }
+
+  updateRangeText() {
+    if (this.startDate && this.endDate) {
+      const start = this.startDate.toLocaleDateString('hu-HU');
+      const end = this.endDate.toLocaleDateString('hu-HU');
+      this.rangeText = `${start} - ${end}`;
+    } else {
+      this.rangeText = 'Bérlési időszak kiválasztása';
+    }
+  }
+
 }
 
 
