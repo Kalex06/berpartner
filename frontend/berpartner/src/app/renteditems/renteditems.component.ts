2d25e72dd654378818afb01f041efd42bf7f4243
M-Maros-DA <maros.mate@dozsakalocsa.hu>
2026-02-12 02:19:22 +0100

    Myitems újra dizájnolása, rentedItems felület megírása

--- frontend/berpartner/src/app/renteditems/renteditems.component.ts
+++ frontend/berpartner/src/app/renteditems/renteditems.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-renteditems',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './renteditems.component.css'
 })
 export class RenteditemsComponent {
+  constructor(private router: Router) { }
 
+  navigateBack() {
+    setTimeout(() => {
+      this.router.navigate(['/home']);
+    }, 400);
+  }
 }
