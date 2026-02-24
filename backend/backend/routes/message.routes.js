cde0932c380cfa005da11689739b99bd404d5272
Kalex06 <kopo.alex@dozsakalocsa.hu>
2026-02-24 23:03:19 +0100

    üzenetek alap

---
+++ backend/backend/routes/message.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const authMiddleware = require('../middleware/auth.middleware');
+
+const message_router = express.Router();
+
+
+
+module.exports = message_router;
