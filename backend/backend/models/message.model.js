cfa77a1d6c8b0407b800c9539b075593e539e86f
Kalex06 <kopo.alex@dozsakalocsa.hu>
2026-02-25 09:25:51 +0100

    üzenetek feltöltése

--- backend/backend/models/message.model.js
+++ backend/backend/models/message.model.js
@@ -1,13 +1,26 @@
 const pool = require('../config/db');
 
 
-async function createMessage(data) {
-    const {felado_id,cimzett_id,cim,tartalom,tipus} = data;
+async function createMessage(pool,data) {
+    const {felado_id,cimzett_id,eszkoz_id,cim,tartalom,tipus} = data;
     const [row] = await pool.execute(
-        'INSERT INTO uzenetek (felado_id,cimzett_id,cim,tartalom,tipus) VALUES (?, ?, ?, ?, ?)',
-        [felado_id,cimzett_id,cim,tartalom,tipus]
+        'INSERT INTO uzenetek (felado_id,cimzett_id,eszkoz_id,cim,tartalom,tipus) VALUES (?, ?,?, ?, ?, ?)',
+        [felado_id,cimzett_id,eszkoz_id,cim,tartalom,tipus]
     );
     return row.insertId;
 }
 
-module.exports = {createMessage};
+
+async function getAllMessageByOwner(id) {
+    const[row] = await pool.execute(
+    'SELECT * FROM uzenetek WHERE uzenetek.cimzett_id = (?)',
+    [id]
+    );
+    return row
+}
+
+
+
+
+
+module.exports = {createMessage,getAllMessageByOwner};
