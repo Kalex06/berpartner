757b104e13684b1cbeeae10481e623c78152f40b
Kalex06 <kopo.alex@dozsakalocsa.hu>
2026-02-25 09:25:51 +0100

    üzenetek feltöltése

--- backend/backend/controllers/message.controller.js
+++ backend/backend/controllers/message.controller.js
@@ -2,13 +2,20 @@ const Message = require('../models/message.model');
 
 
 async function getAllMessagesByOwner(req,res) {
+
     try{
+        
+        const message = Message.getAllMessageByOwner(req.user.id);
+        
+        
+
 
     }
     
     catch(err){
-
+             res.status(500).json({message:"Hiba feltöltés közben",error: err.message });
     }
+    
     
 }
 
