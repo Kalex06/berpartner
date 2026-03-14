89cb09a77fa059a8b7854905ed0d957d39e6b0aa
Kalex06 <kopo.alex@dozsakalocsa.hu>
2026-03-14 20:50:23 +0100

    user service + delete

---
+++ frontend/berpartner/src/app/services/user/user.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private API = 'http://localhost:3000';
+  
+    constructor(private http: HttpClient) {}
+
+    updatePhone(data:any){
+   return this.http.patch<any>(`${this.API}/user/update/phone`,data);
+  }
+
+  updateUsername(data:any){
+   return this.http.patch<any>(`${this.API}/user/update/username`,data);
+  }
+
+  updateEmail(data:any){
+   return this.http.patch<any>(`${this.API}/user/update/email`,data);
+  }
+
+  updateProfilePicture(data:any){
+   return this.http.patch<any>(`${this.API}/user/update/avatar`,data);
+  }
+ 
+  deleteUser(id:number){
+   return this.http.delete<any>(`${this.API}/user/delete/${id}`)
+  }
+  
+}
