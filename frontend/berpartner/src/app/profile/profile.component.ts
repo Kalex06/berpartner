fd7ceb3ede1171d78692c653f185d1337e35990c
M-Maros-DA <maros.mate@dozsakalocsa.hu>
2026-03-26 01:54:45 +0100

    Profil oldalon profilkép választás + Hover Overlay, a választás nincs kész

--- frontend/berpartner/src/app/profile/profile.component.ts
+++ frontend/berpartner/src/app/profile/profile.component.ts
@@ -3,6 +3,8 @@ import { AuthService } from '../services/auth/auth.service';
 import { ItemService } from '../services/item/item.service';
 import { switchMap } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../services/user/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-profile',
@@ -10,10 +12,9 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  constructor(private auth: AuthService, private item: ItemService, private route: ActivatedRoute) { }
+  constructor(private auth: AuthService, private item: ItemService, private route: ActivatedRoute, private _user: UserService, private toastr: ToastrService) { }
   user: any = null
   posts: any[] = [];
-
 
   ngOnInit() {
     this.auth.getProfile(Number(this.route.snapshot.paramMap.get('id'))).pipe(
@@ -29,6 +30,31 @@ export class ProfileComponent {
         console.log(err)
       },
     });
+  }
+
+  selectedFile: File | null = null;
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      const formData = new FormData();
+
+      if (this.selectedFile) {
+        formData.append('selectedFile', this.selectedFile, this.selectedFile.name);
+
+        this._user.updateProfilePicture(formData).subscribe({
+          next: () => this.toastr.success("Sikeres Profilkép módosítás!"),
+          error: (err) => {
+            this.toastr.error("Sikertelen Profilkép módosítás!")
+          }
+        })
+      }
+    }
   }
 
 }
