553ca4217edd8b87c58c9ca0bdd31b5284a9c4a5
Kalex06 <kopo.alex@dozsakalocsa.hu>
2026-04-02 22:50:44 +0200

    bejelentkezés + navbár

--- berpartner admin desktop/berpartner admin/MainWindow.xaml.cs
+++ berpartner admin desktop/berpartner admin/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;          
+using System.Text.Json;     
+using System.Threading.Tasks; 
 
 namespace berpartner_admin
 {
@@ -19,6 +22,67 @@ namespace berpartner_admin
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public class Login {
+        
+            public string email { get; set; }
+            public string jelszo { get; set; }
+        
+        }
+
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Login loginData = new Login();
+            loginData.email = emailInput.Text;
+            loginData.jelszo = passwordInput.Password;
+
+
+            using (HttpClient client = new HttpClient()) {
+
+                try
+                {
+                    string json = JsonSerializer.Serialize(loginData);
+                    StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage respons = await client.PostAsync("http://localhost:3000/auth/login", content);
+
+                    if (respons.IsSuccessStatusCode)
+                    {
+
+                        string responsContent = await respons.Content.ReadAsStringAsync();
+
+
+                        using (JsonDocument doc = JsonDocument.Parse(responsContent)) {
+                        
+                            JsonElement root = doc.RootElement;
+
+                            App.Token = root.GetProperty("token").GetString();
+
+                        }
+
+                            
+
+                            HomeWindow home = new HomeWindow();
+                        home.Show();
+                        this.Close();
+
+                    }
+                    else
+                    {
+
+                        MessageBox.Show("Hibás felhasználónév vagy jeszó!", "Hibás Adatok", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                    }
+
+                }
+                catch (Exception ex) {
+
+                    MessageBox.Show(ex.ToString(), "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
         }
     }
 }
