0ce5b02007f68f706a9272b74610c96d1afca727
M-Maros-DA <maros.mate@dozsakalocsa.hu>
2026-04-09 18:00:59 +0200

    Értesítések & Navigációs sáv reszponzivitás javítás, értesítések css javítás

--- frontend/berpartner/src/app/notifications/notifications.component.ts
+++ frontend/berpartner/src/app/notifications/notifications.component.ts
@@ -8,60 +8,61 @@ import { Router } from '@angular/router';
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css'
 })
-export class NotificationsComponent implements OnInit{
+export class NotificationsComponent implements OnInit {
 
-  constructor(private Message:MessageService,private router:Router){}
+  constructor(private Message: MessageService, private router: Router) { }
 
+  isMenuOpen = true;
 
-messages:any[] = []
+  messages: any[] = []
 
   ngOnInit(): void {
 
     this.load();
-    
+
   }
 
- load(){
-  this.Message.getMessages().subscribe({
-      next:(data)=>{
+  load() {
+    this.Message.getMessages().subscribe({
+      next: (data) => {
         this.messages = data;
-        
+
       },
-      error:(err: HttpErrorResponse)=> {
-        if(err.status == 403 || err.status == 401){
+      error: (err: HttpErrorResponse) => {
+        if (err.status == 403 || err.status == 401) {
           this.router.navigate(['/login']);
-  
+
         }
       }
     })
- }
+  }
 
-  accept(data: any){
+  accept(data: any) {
     this.Message.acceptMessage(data).subscribe({
-      next:()=>{
+      next: () => {
         console.log("Kérés elfogadva");
         this.load();
       },
-      error:(err: HttpErrorResponse)=> {
-        if(err.status == 403 || err.status == 401){
+      error: (err: HttpErrorResponse) => {
+        if (err.status == 403 || err.status == 401) {
           this.router.navigate(['/login']);
-  
+
         }
       }
     })
 
   }
 
-  reject(data:any){
+  reject(data: any) {
     this.Message.rejectMessage(data).subscribe({
-      next:()=>{
+      next: () => {
         console.log("Kérés elutasítva");
         this.load();
       },
-      error:(err: HttpErrorResponse)=> {
-        if(err.status == 403 || err.status == 401){
+      error: (err: HttpErrorResponse) => {
+        if (err.status == 403 || err.status == 401) {
           this.router.navigate(['/login']);
-  
+
         }
       }
     })
