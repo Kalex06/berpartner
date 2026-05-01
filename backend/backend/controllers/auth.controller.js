d50df5cc5d5713abedaeb72a28111872b1db771b
Kalex06 <kopo.alex@dozsakalocsa.hu>
2026-05-02 01:49:58 +0200

    javítások

--- backend/backend/controllers/auth.controller.js
+++ backend/backend/controllers/auth.controller.js
@@ -104,7 +104,12 @@ async function regist(req, res) {
         const id = await User.createUser(user);
         res.status(201).json({ message: 'Sikeres regisztráció', id });
     } catch (err) {
+        if(err.code === '1062'){
+            res.status(409).json({ message: 'A megadott e-mail cím már foglalt!'});
+        }
+        else{
         res.status(500).json({ message: 'Hiba a felhasználó regisztrálásakor', error: err.message });
+    }
     }
 }
 
